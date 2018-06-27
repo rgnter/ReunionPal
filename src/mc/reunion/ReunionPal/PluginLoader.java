20bc06b21f4f6887b3548d358068344588bb30f0
Plugins <wener.zero@gmail.com>
2018-06-27 17:05:21 +0200

    The basics again

--- src/mc/reunion/ReunionPal/PluginLoader.java
+++ src/mc/reunion/ReunionPal/PluginLoader.java
@@ -1,18 +1,19 @@
 package mc.reunion.ReunionPal;
 
-import mc.reunion.ReunionPal.model.Reference;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginLoader extends JavaPlugin {
+    // Prefix
+    public static final String PREFIX = "§c§lReunionPal §7>> ";
+
     // On plugin load
     @Override
     public void onLoad() {
-        info("Loading plugin...");
+        send("Loading plugin...");
 
         // Reference init
-        debug("Initializing References...");
-        Reference.onLoad(this);
+        send("Initializing References...");
 
 
         // TODO Cokolvek čo chceš načítať
@@ -21,22 +22,18 @@ public class PluginLoader extends JavaPlugin {
     // On plugin enable
     @Override
     public void onEnable() {
-        info("Plugin enabled!");
+        send("Plugin enabled!");
         System.out.println();
-        debug("Just some developer informations...");
-        debug("Version: " + this.getDescription().getVersion());
-        debug("Main: " + this.getDescription().getMain());
-        debug("Data folder path: "+Reference.DATA_FOLDER_PATH);
+        send("Just some developer informations...");
+        send("Version: " + this.getDescription().getVersion());
+        send("Main: " + this.getDescription().getMain());
+        send("Data folder path: "+ this.getDataFolder().getPath());
         System.out.println();
     }
 
     // Sends colorized message, will be more visible in console
-    public static void info(String msg) {
+    public static void send(String msg) {
         msg = msg.trim();
-        Bukkit.getServer().getConsoleSender().sendMessage(Reference.PREFIX + " §fINFO >>"+ " §f" + msg);
-    }
-    public static void debug(String msg) {
-        msg = msg.trim();
-        Bukkit.getServer().getConsoleSender().sendMessage(Reference.PREFIX + "§7" + msg);
+        Bukkit.getServer().getConsoleSender().sendMessage(PREFIX + " §fINFO >>"+ " §f" + msg);
     }
 }
